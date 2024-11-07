515c34a3527f037e60519d0f427d2bc007333203
empressv <145819651+empressv@users.noreply.github.com>
2024-11-07 01:25:40 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
 
 	<title>Ubuntu Server-PHP Deployment</title>
 </head>
+<style>
+    
+</style>
 
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <body>
-
+<form action="/submit" method="POST">
 <p>Ubuntu Server-PHP deployment - Lovely Vanessa</p>
   
    <div> 
@@ -28,8 +31,23 @@
      <input type="text" name="lastname">  
    </div>
 
-   
+   <div> 
+     <label>Age:</label>
+     <input type="text" name="lastname">  
+   </div>
 
+   <div> 
+     <label>Address:</label>
+     <input type="text" name="lastname">  
+   </div>
+
+   <div> 
+     <label>Course & Section:</label>
+     <input type="text" name="lastname">  
+   </div>
+
+   <button type="submit">Submit</button>
+   </form>
 </body>
 </html>
 
